3d0d4a7cb72682bfc620b54b17f685b606eedeac
thunderchen <853524319@qq.com>
2026-07-07 17:21:10 +0800

    refactor(useUpdater & 入口文件): 重构模块结构，整理导出写法
    将useUpdater的核心实现抽离至独立的useUpdater.ts文件，优化根目录入口文件的导出语句，统一模块导出规范，提升代码可维护性

--- index.ts
+++ index.ts
@@ -1,17 +1,11 @@
-export {
-  default as useGeolocation,
-  transformWGS84ToGCJ02,
-} from './useGeolocation/useGeolocation';
-export type {
-  PositionState,
-  UseGeolocationOptions,
-} from './useGeolocation/useGeolocation';
+export * from './useGeolocation';
+export * from './useProviderInject';
+export * from './useUpdater';
 
 export { default as useHighPrecisionTimer } from './useHighPrecisionTimer';
 export type { optionsType as HighPrecisionTimerOptions } from './useHighPrecisionTimer/hp-time';
-
-export { useInjectContext, useProviderContext } from './useProviderInject';
-export type { argsType as ProviderContextConfig, ataticType as ProviderContextType } from './useProviderInject';
-
-export { useUpdater } from './useUpdater';
-export type { Options as UpdaterOptions } from './useUpdater/plugin';
+export type {
+  argsType as ProviderContextConfig,
+  ataticType as ProviderContextType,
+} from './useProviderInject';
+export type { Options as UpdaterOptions } from './useUpdater';
